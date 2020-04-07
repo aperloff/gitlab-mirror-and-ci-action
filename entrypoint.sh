fedd1b715ad771732b4c851b923a641895d8fcc0
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-04-07 13:43:00 -0500

    Update entrypoint.sh
    Adding an option to define which branch to checkout. This is necessary when running this workflow from a pull_request event.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ set -u
 DEFAULT_POLL_TIMEOUT=10
 POLL_TIMEOUT=${POLL_TIMEOUT:-$DEFAULT_POLL_TIMEOUT}
 
-git checkout "${GITHUB_REF:11}"
+DEFAULT_GITHUB_REF=${GITHUB_REF:11}
+git checkout "${CHECKOUT_BRANCH:-$DEFAULT_GITHUB_REF}"
 
 branch=$(git symbolic-ref --short HEAD)
 
