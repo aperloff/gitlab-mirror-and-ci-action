a74b8942d2764cb3d7eb6858afe55d471981f9bb
Robért Glein <38926379+RobertGlein@users.noreply.github.com>
2020-04-20 11:45:56 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
 sh -c "git remote add mirror $*"
 sh -c "echo pushing to $branch branch at $(git remote get-url --push mirror)"
-sh -c "git push mirror $branch"
+git push mirror $branch
 
 if [[ -z "${GITLAB_PASSWORD}" ]]; then
   echo "Empty or non-existent GitLab password."
